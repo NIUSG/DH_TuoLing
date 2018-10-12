eef3d923dd989921817a2dbbca499d38ef5503e3
NiuShao.Lenovo <370574131@qq.com>
2018-10-12 11:15:54 +0800

    修改db，发布文档

--- dev_doc/version-1.1.1/visit_cache.sql
+++ dev_doc/version-1.1.1/visit_cache.sql
@@ -6,3 +6,4 @@ create table ns_visit_cache(
 )engine=innodb default charset=utf8 comment='访问统计临时表';
 
 alter table ns_visitor_log add vst_date timestamp not null default "0000-00-00 00:00:00" after vst_at;
+alter table ns_visitor_log modify vst_url text not null;
