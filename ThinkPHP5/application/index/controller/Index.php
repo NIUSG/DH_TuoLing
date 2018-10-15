aa4ebb00938efb3dd36106161fa159f6844b290a
NiuShao.Lenovo <370574131@qq.com>
2018-10-15 18:47:06 +0800

    日志

--- ThinkPHP5/application/index/controller/Index.php
+++ ThinkPHP5/application/index/controller/Index.php
@@ -2,21 +2,38 @@
 namespace app\index\controller;
 use think\Db;
 use app\visit\controller\Write;
+use app\traits\logger\LoggerTrits;
 class Index extends Common
 {
+    use LoggerTrits;
     public $class_fid_TechBlog = 2;
     public function index()
     {
+$this->logger_init('index_page');
+$this->info('start...');
+$time1 = microtime(true);
         Write::writeDB();
+$time2 = microtime(true);
         //首页推荐
         $home_blog_list = $this->blog_home();
+$time3 = microtime(true);
         //博客点击排行
         $this->blog_click();
+$time4 = microtime(true);
         //博客最新发布
         $this->blog_new();
+$time5 = microtime(true);
         //常用链接
         $this->link_often();
+$time6 = microtime(true);
+$write_db = $time2-$time1;
+$blog_list = $time3-$time2;
+$blog_click = $time4-$time3;
+$blog_new = $time5-$time4;
+$link_often = $time6-$time5;
+$this->info("[write_db][{$write_db}][blog_list][{$blog_list}][blog_click][{$blog_click}][blog_new][{$blog_new}][link_often][{$link_often}]");
         $this->assign('home_blog_list',$home_blog_list);
+
         return $this->fetch();
     }
     //首页推荐博客10篇//技术博客,浏览量从上往下
