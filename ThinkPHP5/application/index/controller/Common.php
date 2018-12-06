7b16e5c379e167b2fd96c3f28affdf286e8db4e3
NiuShao.Ali <370574131@qq.com>
2018-12-06 10:46:38 +0800

    文件缓存表头

--- ThinkPHP5/application/index/controller/Common.php
+++ ThinkPHP5/application/index/controller/Common.php
@@ -5,19 +5,55 @@ use think\Db;
 use think\Url;
 use app\visit\controller\Write;
 use app\helper\controller\GetIp;
+use think\cache\driver\File;
 class Common extends controller
 {
-    private $pre = "visit_con";
+    private $pre_avoid_refresh = "avoid_refresh-";
     private $top_class_key = "top_class_key";
+    private $top_class_expire_time = "2592000";
+    private $top_class_is_cache = true;
+    public $file_obj;
     public function _initialize()
     {
+        $this->file_obj = new File();
+        $this->avoid_refresh_by_session();
         //查询网站顶部栏标题并加载
+        if($this->top_class_is_cache && $this->file_obj->has($this->top_class_key)){
+            $top_class_list = $this->file_obj->get($this->top_class_key);
+        }else{
+            $top_class_list = $this->get_top_class_list();
+            $this->file_obj->set($this->top_class_key,$top_class_list,$this->top_class_expire_time);
+        }
+        $this->assign('top_class_list',$top_class_list);
+        return $this->fetch('public/header');
+    }
+    private function get_top_class_list()
+    {
         $top_class_sql = "select class_id,class_title,class_Etitle from ns_class where class_fid = 0 and class_status = 1 order by class_oid desc";
         $top_class_list = Db::query($top_class_sql);
         foreach($top_class_list as $key => $val){
             $top_class_list[$key]['path_url'] = "http://".$_SERVER['HTTP_HOST'].Url::build('index/'.$val["class_Etitle"].'/index');;
         }
-        $this->assign('top_class_list',$top_class_list);
-        return $this->fetch('public/header');
+        return $top_class_list;
+    }
+    private function avoid_refresh_by_session()
+    {
+        session_start();
+        $cur_time = time();
+        if(isset($_SESSION['last_time'])){
+            $_SESSION['click_refresh'] += 1;
+        }else{
+            $_SESSION['click_refresh'] = 1;
+            $_SESSION['last_time'] = time();
+        }
+        if($cur_time - $_SESSION['last_time'] <= 10){
+            if($_SESSION['click_refresh'] >=30){
+                die('点击过快,请慢点刷新');
+            }
+        }else{
+            $_SESSION['last_time'] = $cur_time;
+            $_SESSION['click_refresh'] = 0;
+        }
+        session_destroy();
     }
 }
---
+++ ThinkPHP5/runtime/cache/c6/78931178efc56ac323ce590b578af1.php
@@ -0,0 +1,4 @@
+<?php
+//000002592000
+ exit();?>
+a:3:{i:0;a:4:{s:8:"class_id";i:1;s:11:"class_title";s:3:"Web";s:12:"class_Etitle";s:4:"link";s:8:"path_url";s:69:"http://112.74.31.36/DH_TuoLing/ThinkPHP5/public/index/link/index.html";}i:1;a:4:{s:8:"class_id";i:2;s:11:"class_title";s:4:"Blog";s:12:"class_Etitle";s:4:"blog";s:8:"path_url";s:69:"http://112.74.31.36/DH_TuoLing/ThinkPHP5/public/index/blog/index.html";}i:2;a:4:{s:8:"class_id";i:3;s:11:"class_title";s:7:"AboutMe";s:12:"class_Etitle";s:7:"aboutme";s:8:"path_url";s:72:"http://112.74.31.36/DH_TuoLing/ThinkPHP5/public/index/aboutme/index.html";}}
