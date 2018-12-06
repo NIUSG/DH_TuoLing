6886177461fbb63a262446c7a516acefe92dac07
NiuShao.Ali <370574131@qq.com>
2018-12-06 11:09:52 +0800

    首页缓存已添加

--- ThinkPHP5/application/index/controller/Index.php
+++ ThinkPHP5/application/index/controller/Index.php
@@ -7,6 +7,11 @@ class Index extends Common
 {
     use LoggerTrits;
     public $class_fid_TechBlog = 2;
+    private $blog_home_list_key = "blog_home_list_key";
+    private $blog_click_key = "blog_click_key";
+    private $blog_new_key = "blog_new_key";
+    private $link_often_key = "link_often_key";
+    private $cache_time = 86400;
     public function index()
     {
 $this->logger_init('index_page');
@@ -39,7 +44,10 @@ $this->info("[write_db][{$write_db}][blog_list][{$blog_list}][blog_click][{$blog
     //首页推荐博客10篇//技术博客,浏览量从上往下
     public function blog_home()
     {
-        $sql = "SELECT bloginfo_id,bloginfo_title,bloginfo_describe,bloginfo_img,from_unixtime(bloginfo_createtime,'%Y-%m-%d') bloginfo_createtime,class_title
+        if($this->is_cache && $this->file_obj->has($this->blog_home_list_key)){
+            $res = $this->file_obj->get($this->blog_home_list_key);
+        }else{
+            $sql = "SELECT bloginfo_id,bloginfo_title,bloginfo_describe,bloginfo_img,from_unixtime(bloginfo_createtime,'%Y-%m-%d') bloginfo_createtime,class_title
                 FROM ns_bloginfo AS nb
                 LEFT JOIN ns_class AS nc ON nc.class_id = nb.class_id
                 WHERE nb.bloginfo_status = 1 AND nb.class_id IN (SELECT class_id
@@ -47,44 +55,61 @@ $this->info("[write_db][{$write_db}][blog_list][{$blog_list}][blog_click][{$blog
                                                                  WHERE class_status = 1 AND class_fid = {$this->class_fid_TechBlog})
                 ORDER BY nb.bloginfo_click DESC
                 LIMIT 10";
-        $res = Db::query($sql);
+            $res = Db::query($sql);
+            $this->file_obj->set($this->blog_home_list_key,$res,$this->cache_time);
+        }
         return $res;
     }
     //点击排行技术类博客前十条,并且加载
     public function blog_click()
     {
-        $sql = "SELECT bloginfo_id,bloginfo_title,bloginfo_click
+        if($this->is_cache && $this->file_obj->has($this->blog_click_key)){
+            $res = $this->file_obj->get($this->blog_click_key);
+        }else{
+            $sql = "SELECT bloginfo_id,bloginfo_title,bloginfo_click
                 FROM ns_bloginfo
                 WHERE bloginfo_status = 1 and class_id IN (SELECT class_id
                                                            FROM ns_class
                                                            WHERE class_status = 1 AND class_fid = {$this->class_fid_TechBlog})
                 ORDER BY bloginfo_click DESC
                 LIMIT 10";
-        $res = Db::query($sql);
+            $res = Db::query($sql);
+            $this->file_obj->set($this->blog_click_key,$res,$this->cache_time);
+        }
         $this->assign('blog_click_list',$res);
     }
     //最新技术类发布前十条
     public function blog_new()
     {
-        $sql = "SELECT bloginfo_id,bloginfo_title,bloginfo_click
+        if($this->is_cache && $this->file_obj->has($this->blog_new_key)){
+            $res = $this->file_obj->get($this->blog_new_key);
+        }else{
+            $sql = "SELECT bloginfo_id,bloginfo_title,bloginfo_click
                 FROM ns_bloginfo
                 WHERE bloginfo_status = 1 and class_id IN (SELECT class_id
                                                            FROM ns_class
                                                            WHERE class_status = 1 AND class_fid = {$this->class_fid_TechBlog})
                 ORDER BY bloginfo_createtime DESC
                 LIMIT 5";
-        $res = Db::query($sql);
+            $res = Db::query($sql);
+            $this->file_obj->set($this->blog_new_key,$res,$this->cache_time);
+        }
         $this->assign('blog_new_list',$res);
     }
     //常用导航
     public function link_often()
     {
-        $sql = "SELECT link_id,link_title,link_url
+        if($this->is_cache && $this->file_obj->has($this->link_often_key)){
+            $res = $this->file_obj->get($this->link_often_key);
+        }else{
+            $sql = "SELECT link_id,link_title,link_url
                 FROM ns_link
                 WHERE link_status = 1
                 ORDER BY link_clicknum DESC
                 LIMIT 20";
-        $res = Db::query($sql);
+            $res = Db::query($sql);
+            $this->file_obj->set($this->link_often_key,$res,$this->cache_time);
+        }
         $this->assign('link_often_list',$res);
 
     }
