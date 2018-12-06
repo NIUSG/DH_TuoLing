d09fb16838ba1f107b8dfa1a48d53795de92fe94
NiuShao.Ali <370574131@qq.com>
2018-12-06 09:23:31 +0800

    代码回滚

--- ThinkPHP5/application/index/controller/Common.php
+++ ThinkPHP5/application/index/controller/Common.php
@@ -8,9 +8,9 @@ use app\helper\controller\GetIp;
 class Common extends controller
 {
     private $pre = "visit_con";
+    private $top_class_key = "top_class_key";
     public function _initialize()
     {
-        $this->visit_con();
         //查询网站顶部栏标题并加载
         $top_class_sql = "select class_id,class_title,class_Etitle from ns_class where class_fid = 0 and class_status = 1 order by class_oid desc";
         $top_class_list = Db::query($top_class_sql);
@@ -19,27 +19,5 @@ class Common extends controller
         }
         $this->assign('top_class_list',$top_class_list);
         return $this->fetch('public/header');
-    }
-    private function visit_con()
-    {
-        $ip_info = json_decode(GetIp::get_ip(),true);
-        $ip = $ip_info['data'];
-        $key = $this->get_visit_con_key($ip);
-        $redis_obj = new \Redis;
-        $redis_obj->connect('127.0.0.1', 6379);
-        if($redis_obj->exists($key)){
-            $redis_obj->incr($key);
-        }else{
-            $redis_obj->setex($key,10,1);
-        }
-        $num = $redis_obj->get($key);
-        if($num>20){
-            die('点击过于频繁，清稍等再刷');
-        }
-    }
-    private function get_visit_con_key($ip)
-    {
-        $key = $this->pre."-".$ip;
-        return $key;
     }
 }
