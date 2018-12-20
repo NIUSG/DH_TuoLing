063591c3c70998efe55b3e882b58d830c3928a11
NiuShao.TcECS <370574131@qq.com>
2018-12-20 19:21:34 +0800

    搜索日志表重置

--- dev_doc/version-1.1.2/search_log.sql
+++ dev_doc/version-1.1.2/search_log.sql
@@ -1,7 +1,7 @@
 create table if not exists `ns_search_log`(
 `id` int unsigned not null primary key auto_increment,
-`search_type` varchar(64) not null,
-`is_cache` tinyint not null,
+`search_type` enum("db","cache") not null,
+`is_cache` enum("0","1") not null,
 `search_time` varchar(255) not null,
 `search_key` varchar(255) not null,
 `created_at` datetime not null,
