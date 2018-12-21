272a1a38d394fdcbc2d19d024bc1ef6b80824a71
NiuShao.TcECS <370574131@qq.com>
2018-12-21 15:13:21 +0800

    monolog的定义和初始使用

--- item/application/tools/controller/Log.php
+++ item/application/tools/controller/Log.php
@@ -1,10 +1,19 @@
 <?php
 namespace app\tools\controller;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class Log
 {
+    public $log_path_monogo = LOG_PATH.'monolog/';
     public function init()
     {
-        echo "init";
+        $log = new Logger('abc');
+        var_dump($this->log_path_monogo);
+        $log->pushHandler(new StreamHandler($this->log_path_monogo.'/abc.log', Logger::WARNING));
+        // add records to the log
+        $log->warning('Foo');
+        $log->error('Bar');
+        $log->info('abc');
     }
 }
