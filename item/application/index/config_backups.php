8114e5da8f9bc7cd222f0fd5034aa3cc697600a2
NiuShao.Ali.Ecs <370574131@qq.com>
2019-03-13 15:09:01 +0800

    重置.gitignore

---
+++ item/application/index/config_backups.php
@@ -0,0 +1,9 @@
+<?php
+//配置文件
+return [
+    'view_replace_str' => [
+        '__HOME_STYLE__' => 'http://127.0.0.1/DH_TuoLing/item/public/static/IndexStyle',
+        '__HOMEPAGE_IMG__' => 'http://127.0.0.1/DH_TuoLing/item/public/uploads/HomepageImg',
+        '__BLOG_IMG__'=> 'http://127.0.0.1/DH_TuoLing/item/public/uploads/BlogsImg'
+    ],
+];
