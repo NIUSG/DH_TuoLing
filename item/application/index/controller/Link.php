d9e85a6d309017c974534359156141d2e60a1f9c
NiuShao.Ali.Ecs <370574131@qq.com>
2019-03-14 16:56:23 +0800

    link也面完成,首页url有bug需要修改

--- item/application/index/controller/Link.php
+++ item/application/index/controller/Link.php
@@ -2,29 +2,37 @@
 namespace app\index\controller;
 use app\index\controller\Common;
 use app\index\model\LinkModel;
+use app\index\model\CategoryModel;
 class Link extends Common
 {
-    private $M_Link;
+    public $M_link;
+    public $M_class;
+    public $link_class = 1;
     public function __construct()
     {
         parent::__construct();
-        $this->M_Link = new LinkModel();
+        $this->M_link = new LinkModel;
+        $this->M_class = new CategoryModel;
     }
     public function index()
     {
-        $list = [];
-        $list['recommend_link'] = $this->M_Common->get_link_clicknum(30);
-        list(,,$list['class_link_info']) = $this->M_Link->get_class_link_info();
+        $list['class_link_info'] = $this->get_class_link_info();
+        $list['recommend_link'] = $this->M_link->get_link_clicknum(35);
         $list['right_list'] = $this->get_right_list();
         $this->assign("list",$list);
         return $this->fetch();
     }
-    public function click_num()
+    public function get_class_link_info()
     {
-        $url = input('get.url');
-        $link_id = input('get.id');
-        //访问历史存入缓存中
-        $this->M_Link->record_visit_link($link_id);
-        $this->redirect($url);
+        $link_info = $this->M_link->get_link_info('link_id,link_title,link_url,class_id');
+        $class_info = $this->M_class->get_class_list_by_fid($this->link_class,'class_id,class_title');
+        $class_link_info = array_map( function($v) use ($link_info){
+                $v['link_info'] = [];
+                foreach($link_info  as $val){
+                    if($v['class_id'] == $val['class_id']) $v['link_info'][] = $val;
+                }
+                return $v;
+        } ,$class_info);
+        return $class_link_info;
     }
 }
--- item/application/index/model/LinkModel.php
+++ item/application/index/model/LinkModel.php
@@ -4,7 +4,7 @@ use think\Model;
 use think\Db;
 class LinkModel extends CommonModel
 {
-    public function get_link_clicknum()
+    public function get_link_clicknum($limit = 20)
     {
         $sql = "SELECT
                         link_id,link_title,link_url
@@ -14,7 +14,20 @@ class LinkModel extends CommonModel
                         link_status = 1
                 ORDER BY
                         link_clicknum DESC
-                LIMIT 20";
+                LIMIT {$limit}";
+        $res = Db::query($sql);
+        return $res;
+    }
+    public function get_link_info($field = "*")
+    {
+        $sql = "SELECT
+                        {$field}
+                FROM
+                        ns_link
+                WHERE
+                        link_status = 1
+                ORDER BY
+                        link_clicknum DESC";
         $res = Db::query($sql);
         return $res;
     }
