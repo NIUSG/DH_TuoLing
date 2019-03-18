55504e0c7863967bd7db3e878d9ab80de993db6a
NiuShao.Ali.Ecs <370574131@qq.com>
2019-03-18 19:40:18 +0800

    测试脚本准备完毕

--- item/application/command/command/Test.php
+++ item/application/command/command/Test.php
@@ -1,7 +1,17 @@
 <?php
 namespace app\command\command;
-
-class Test
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use app\tools\controller\Encrypt;
+class Test extends Command
 {
-
+    protected function configure()
+    {
+        $this->setName('Test')->setDescription('测试脚本');
+    }
+    protected function execute(Input $input, Output $output)
+    {
+        var_dump("test");
+    }
 }
