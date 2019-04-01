97ce9327793c4a5d3dd5975824783bd59fd1645e
niushaogang <niushaogang@digifinex.com>
2019-04-01 21:23:26 +0800

    完善文档，开发代码

--- DownloadCenter/item/command/refreshDir.php
+++ DownloadCenter/item/command/refreshDir.php
@@ -1,6 +1,11 @@
 <?php
+//文件中心
 $dir = dirname(dirname(dirname(__FILE__)));
 $resource = $dir.'/resource/';
+//文件中心是否存在
 if(!is_dir($resource)){
   var_dump('NOT EXISTS');
+  die();
 }
+$file = scandir($resource);
+var_dump($file);die();
