90168d20e94ef7d694b2bc86caee39d188d84d5a
NiuShao.LinodeVps.JP <370574131@qq.com>
2019-04-04 23:37:19 +0800

    .gitignore

--- DownloadCenter/item/core/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'dh_tuoling');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('RESOURCE_DOMAIN','http://localhost:8080/DH_TuoLing/DownloadCenter/resource');
