936eead11b2f6ab6ec3a32629bc1ed092346387a
niushaogang <niushaogang@digifinex.com>
2019-04-04 18:15:16 +0800

    下载中心文件

--- DownloadCenter/item/command/refreshDir.php
+++ DownloadCenter/item/command/refreshDir.php
@@ -1,11 +1,26 @@
 <?php
 //文件中心
 $dir = dirname(dirname(dirname(__FILE__)));
+
 $resource = $dir.'/resource/';
 //文件中心是否存在
 if(!is_dir($resource)){
   var_dump('NOT EXISTS');
   die();
 }
-$file = scandir($resource);
-var_dump($file);die();
+
+function myscandir1($path, &$arr) {
+
+    foreach (glob($path) as $file) {
+        if (is_dir($file)) {
+            myscandir1($file . '/*', $arr);
+        } else {
+
+            $arr[] = realpath($file);
+        }
+    }
+}
+
+$arr = [];
+myscandir1('D:/',$arr);
+var_dump($arr);die();
---
+++ DownloadCenter/item/common/config.php
@@ -0,0 +1,8 @@
+<?php
+class functions
+{
+    public static function recursionDir($dir)
+    {
+
+    }
+}
