68b244f9da07f2d1e1453eb86847027a0cfe108d
niushaogang <niushaogang@digifinex.com>
2019-04-04 18:47:23 +0800

    自动加载

---
+++ DownloadCenter/item/core/base.php
@@ -0,0 +1,5 @@
+<?php
+
+define('IS_CLI', PHP_SAPI == 'cli' ? true : false);
+define('CMD_PATH',$itemPath.'/command/refreshDir.php');
+define('APP_PATH',$itemPath.'/app/index.php');
---
+++ DownloadCenter/item/public/index.php
@@ -0,0 +1,7 @@
+<?php
+$curPath = __FILE__;
+$itemPath = dirname(dirname($curPath));
+
+require_once $itemPath.'/core/base.php';
+require_once $itemPath.'/core/config.php';
+require_once $itemPath.'/core/start.php';
