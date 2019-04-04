f89f7d873fb61ec5755db174102b2d13346b8fb6
niushaogang <370574131@qq.com>
2019-04-04 23:33:59 +0800

    配置文件

--- DownloadCenter/item/core/config_backups.php
+++ DownloadCenter/item/core/config_backups.php
@@ -3,3 +3,4 @@ define('DB_HOST', '127.0.0.1');
 define('DB_NAME', '');
 define('DB_USER', '');
 define('DB_PASS', '');
+define('RESOURCE_DOMAIN','http://localhost:8080/DH_TuoLing/DownloadCenter/resource');
