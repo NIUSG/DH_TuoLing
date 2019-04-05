2aa0fbeae2149ed9fc536ce584c93633858ccc29
niushaogang <370574131@qq.com>
2019-04-05 12:24:35 +0800

    资源按照首字母排序

--- DownloadCenter/item/app/exec/App.php
+++ DownloadCenter/item/app/exec/App.php
@@ -13,6 +13,12 @@ class App
         $sourceList = $this->getSource();
         require_once VIEW_PATH.'download.html';
     }
+    /**
+     * [getSource 获取数据库中的资源]
+     * @Author   NiuShao
+     * @DataTime 2019-04-05T12:09:57+0800
+     * @return   [type]                   [description]
+     */
     public function getSource()
     {
         $sql = "select name,cur_path from ns_download_center where status = 1";
@@ -21,6 +27,24 @@ class App
             $v['download_path'] = RESOURCE_DOMAIN.$v['cur_path'];
             return $v;
         },$sourceList);
+        $sourceList = $this->formatSource($sourceList);
         return $sourceList;
     }
+    /**
+     * [formatSource 按照首字母排序]
+     * @Author   NiuShao
+     * @DataTime 2019-04-05T12:10:48+0800
+     * @param    [type]                   $sourceList [description]
+     * @return   [type]                               [description]
+     */
+    public function formatSource($sourceList)
+    {
+        $handlerList = array_column($sourceList,'name');
+        $handlerList = array_map(function($v){
+            return strtolower($v);
+        },$handlerList);
+        array_multisort($handlerList,SORT_ASC,$sourceList);
+        return $sourceList;
+    }
+
 }
